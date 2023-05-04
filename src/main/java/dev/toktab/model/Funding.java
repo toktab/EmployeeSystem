08dc7eeda8402f03c2a7dbc04c852b9c803f290d
toktab <tabagaritoko834@gmail.com>
2023-05-03 20:30:54 -0700

    Created Type Funding

---
+++ src/main/java/dev/toktab/model/Funding.java
@@ -0,0 +1,25 @@
+package dev.toktab.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Entity
+@Table(name = "funding")
+@Data
+@NoArgsConstructor
+public class Funding {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "from")
+    private int from;
+    @Column(name = "to")
+    private int to;
+    @Column(name = "amount")
+    private Double amount;
+    @Column(name = "date")
+    private Date date;
+}
