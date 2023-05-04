55a1a76172fd61ea6ba02b97cf95e7ee5e219016
toktab <tabagaritoko834@gmail.com>
2023-05-03 20:28:16 -0700

    Created Type Budget

---
+++ src/main/java/dev/toktab/model/Budget.java
@@ -0,0 +1,23 @@
+package dev.toktab.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Entity
+@Table(name = "budget")
+@Data
+@NoArgsConstructor
+public class Budget {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    @Column(name = "amount")
+    private Double amount;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "date")
+    private Date date;
+}
